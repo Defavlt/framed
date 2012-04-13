68ee280cc04fa25156a304f6c1a111e06455b877
Marcus Hansson <themoran2@gmail.com>
2012-04-13 15:54:47 +0200

    stuff

--- buildplugins.sh
+++ buildplugins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 git add -A
-git commit -a $1
+git commit -am $1
 git push crm.base
--- crm.php
+++ crm.php
@@ -172,6 +172,7 @@ EOT;
 		error_log("BEFORE CHECK", 0);
 		error_log($message, 0);
 		error_log($object, 0);
+		var_dump($this->observerlist);
 		if (array_key_exists($message, $this->observerlist)) {
 			error_log("BEFORE LOOP", 0);
 			foreach ($this->observerlist as $msg => $objects) {
