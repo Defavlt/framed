24ca2688c7bd7b7e3c31be494b00aadc98ee8c31
Marcus Hansson <themoran2@gmail.com>
2012-04-13 12:44:36 +0200

    Added autoload

--- index.php
+++ index.php
@@ -18,6 +18,14 @@ define("BASE", __DIR__ . DIRECTORY_SEPARATOR);
  */
 define("php", ".php");
 
+function __autoload($name) {
+	
+	if (is_file($name)) {
+		
+		require BASE . $name .php;
+	}
+}
+
 crm::Start();
 
 ?>
