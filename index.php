8ab3fe9595ed5a2a8088e2d1c60a742987e6cd8e
Marcus Hansson <themoran2@gmail.com>
2012-04-13 12:57:02 +0200

    Fixed autoload paths and ns

--- index.php
+++ index.php
@@ -23,9 +23,9 @@ spl_autoload_register("__autoload");
 
 function __autoload($name) {
 	
-	if (is_file($name)) {
+	if (is_file($name . php)) {
 		
-		require_once $name;
+		require_once $name . php;
 	}
 	else {
 		file_put_contents("php://stderr", $name);
