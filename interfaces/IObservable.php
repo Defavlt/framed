ce84677345109ef9bb8999a6f1820e688632d617
Marcus Hansson <themoran2@gmail.com>
2012-04-13 13:14:46 +0200

    Added interfaces

---
+++ interfaces/IObservable.php
@@ -0,0 +1,32 @@
+<?php
+namespace interfaces;
+
+/**
+ * Makes an object eligible for observing.
+ * @author marcus
+ *        
+ */
+interface IObservable {
+	
+	/**
+	 * Sends a message to all registered clients.
+	 * @param string $message
+	 * @param mixed $object
+	 * @param string $id
+	 */
+	function SendMessage($message, $object, $id);
+	
+	/**
+	 * Registers the client with this instance
+	 * @param IObserver $client
+	 */
+	function Register($client);
+	
+	/**
+	 * Unregisters the client from this instance
+	 * @param IObserver $client
+	 */
+	function Unregister($client);
+}
+
+?>
