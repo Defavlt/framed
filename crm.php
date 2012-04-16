c5a9c33cbb61f33cc7ee06fefbc0aee25c9d743f
Marcus Hansson <themoran2@gmail.com>
2012-04-16 14:30:50 +0200

    Removed some debug symbols

--- crm.php
+++ crm.php
@@ -178,6 +178,7 @@ class crm implements IPlugin, IObservable {
 EOT;
 					
 					$class = CONFIGURATION::$PLUGIN_DIR . $single_slash . str_replace ( php, null, $plugin );
+					error_log($class, 0);
 					$instance = new $class ();
 					
 					if ($this->RegisterPlugin ( $instance, $plugin )) {
