dff2d8dc51ea16ca16cdedfa8a6de885155b001d
Marcus Hansson <themoran2@gmail.com>
2012-04-16 10:12:12 +0200

    Removed

---
+++ plugins/index.php
@@ -0,0 +1,55 @@
+<?php
+namespace plugins;
+
+use interfaces\IPlugin;
+use interfaces\IObserver;
+use settings\PLUGIN_VISIBILITY;
+
+/**
+ *
+ * @author marcus
+ *        
+ */
+class index implements IPlugin, IObserver {
+	
+	/**
+	 *
+	 * @see IPlugin::Initialize()
+	 *
+	 */
+	public function Initialize() {
+	
+	}
+	
+	/**
+	 *
+	 * @see IPlugin::Plugin()
+	 *
+	 */
+	public function Plugin() {
+		\crm::Register($this, "");
+		\crm::log("index", IPlugin);
+	}
+	
+	/**
+	 *
+	 * @return PLUGIN_VISIBILITY
+	 *
+	 * @see IPlugin::gVisibility()
+	 *
+	 */
+	public function gVisibility() {
+		return PLUGIN_VISIBILITY::PU;
+	}
+	
+	/**
+	 *
+	 * @see IObserver::Callback()
+	 *
+	 */
+	public function Callback($on, $id, $msg) {
+		echo "index, default page.";
+	}
+}
+
+?>
