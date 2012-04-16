71abc921b40f80d22c95fba76d1b5266bdc05cbd
Marcus Hansson <themoran2@gmail.com>
2012-04-16 14:00:13 +0200

    Removed some debug symbols

--- crm.php
+++ crm.php
@@ -235,6 +235,11 @@ EOT;
 		echo "Trying message '" . $message . "' on object '" . $object . "' with id '" . $id . ". ";
 		echo "Key does" . (array_key_exists($message, $this->observerlist) ? " exist" : "n't exist.") . "\n";
 		
+		if (!array_key_exists($message, $this->observerlist)) {
+			echo $message . "\n";
+			var_dump($this->observerlist);
+		}
+		
 		if (array_key_exists ( $message, $this->observerlist )) {
 			
 			foreach ( $this->observerlist [$message] as $msg => $instance ) {
