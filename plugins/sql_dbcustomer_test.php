3b9b90166c0eca1a5650117643b978ce309525a1
Marcus Hansson <themoran2@gmail.com>
2012-04-25 15:41:05 +0200

    Removed some debug symbols. Querying for certain information now works

--- plugins/sql_dbcustomer_test.php
+++ plugins/sql_dbcustomer_test.php
@@ -70,7 +70,7 @@ class sql_dbcustomer_test implements IPlugin, IObserver {
 	
 	<input type="hidden" name="a" value="dbcall"/>
 	<input name="i" type="text" value="$id" />
-	<input type="text" name="top" placeholder="top" maxlength="3" />
+	<input type="text" name="top" placeholder="top" maxlength="3" width="5" />
 	<input type="submit" />
 </form>
 		
