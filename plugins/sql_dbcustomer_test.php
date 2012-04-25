3a9dc4b03c52487e8add6b7db810aa961689b71a
Marcus Hansson <themoran2@gmail.com>
2012-04-25 16:49:29 +0200

    Started adding automatic loading of GlobalParams matching dbobject

--- plugins/sql_dbcustomer_test.php
+++ plugins/sql_dbcustomer_test.php
@@ -95,10 +95,7 @@ HTML;
 			
 		$option[$customer::OPTION_CMP] = $customer::OPTION_CMP_LIKE;
 
-		echo "top = " . $top . "<br>";
-		echo "top is set: " . (isset($top) && $top != "" ? "true<br><br>" : "false<br><br>");
-
-		if (isset($top)) {
+		if (isset($top) && $top != "") {
 			
 			$option[$customer::OPTION_MAX_RESULTS] = $top;
 		}
