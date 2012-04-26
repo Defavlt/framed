f536f0e0b8d9bd3b8bc8a09fcacc5691ca08a0dc
Marcus Hansson <themoran2@gmail.com>
2012-04-26 13:27:46 +0200

    Started adding automatic loading of GlobalParams matching dbobject

---
+++ db/DBCaseinfo.php
@@ -0,0 +1,28 @@
+<?php
+namespace db;
+
+use db\BaseDBObject;
+
+/**
+ *
+ * @author marcus
+ *        
+ */
+class DBCaseinfo extends BaseDBObject {
+	
+	public $title;
+	public $category;
+	public $owner;
+	public $firstname;
+	public $lastname;
+	public $phone;
+	public $mobilephone;
+	public $email;
+	public $status;
+	public $text;
+	public $date;
+	public $createdby;
+	public $org;
+}
+
+?>
